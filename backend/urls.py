3b6fd80b16e60fa721c172aafb93bfa5042884e0
nicolas azzi <nicolasazzi00@hotmail.com>
2021-04-04 10:00:51 +0300

    fixed all receipts url

--- backend/urls.py
+++ backend/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('account.urls')),
-    path('receipt/', include('receipt.urls')),
+    path('receipts/', include('receipt.urls')),
 ]
